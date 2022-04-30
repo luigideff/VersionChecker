7af18452d6aaedfc7435b2a5818e23d17c802faf
Luigi Fiamoncini <luigideff@outlook.com>
2022-04-30 00:02:00 -0300

    Versão Inicial

---
+++ VerificaVersaoDlls/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerificaVersaoDlls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GetSettings()
+        {
+            textBox1.Text = Properties.Settings.Default.DllPath1;
+        }
+
+        public void SaveSettings()
+        {
+            Properties.Settings.Default.DllPath1 = textBox1.Text;
+            Properties.Settings.Default.Save();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetSettings();
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+        }
+    }
+}
